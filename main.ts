956a303d3109cfffc23276664880890c137e1dc8
zptilden325 <71046087+zptilden325@users.noreply.github.com>
2022-06-15 10:05:40 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,132 @@
+namespace SpriteKind {
+    export const target = SpriteKind.create()
+}
+function GhostFollow () {
+    for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+        if (mySprite.x > value.x) {
+            value.vx = 5
+            animation.runImageAnimation(
+            Ghost,
+            [img`
+                ........................
+                ........................
+                ........................
+                ........................
+                ..........fffff.........
+                ........ff11111f........
+                .......fb111111bf.......
+                ......fbd1111111f.......
+                ......fddd111111df......
+                ......fdddd11111df......
+                ......fddddddd11df......
+                ......fddddddd111f......
+                ......fddddddcf11f......
+                .......fbdddb1111bf.....
+                ........fffcfdb1b1f.....
+                .......ffffffffbfbf.....
+                ....ff.fffffffffff......
+                .....ffffffff...........
+                .....ffffffb1b1f........
+                ......ffffffbfbf........
+                ........................
+                ........................
+                ........................
+                ........................
+                `,img`
+                ........................
+                ........................
+                ........................
+                ........................
+                ..........ffff..........
+                ........ff1111ff........
+                .......fb111111bf.......
+                .......fd1111111f.......
+                ......fdd1111111df......
+                ......fddd111111df......
+                ......fdddddd111df......
+                ......fbddddbfd1df......
+                ......fcbbbdcfddbf......
+                .......fcbb11111f.......
+                ........fffff1b1f.......
+                ........fb111cfbf.......
+                ........ffb1b1ff........
+                ......f.fffbfbf.........
+                ......ffffffff..........
+                .......fffff............
+                ........................
+                ........................
+                ........................
+                ........................
+                `],
+            500,
+            true
+            )
+        } else {
+            value.vx = -5
+            animation.runImageAnimation(
+            Ghost,
+            [img`
+                ........................
+                ........................
+                ........................
+                ........................
+                ..........ffff..........
+                ........ff1111ff........
+                .......fb111111bf.......
+                .......f1111111df.......
+                ......fd1111111ddf......
+                ......fd111111dddf......
+                ......fd111ddddddf......
+                ......fd1dfbddddbf......
+                ......fbddfcdbbbcf......
+                .......f11111bbcf.......
+                .......f1b1fffff........
+                .......fbfc111bf........
+                ........ff1b1bff........
+                .........fbfbfff.f......
+                ..........ffffffff......
+                ............fffff.......
+                ........................
+                ........................
+                ........................
+                ........................
+                `,img`
+                ........................
+                ........................
+                ........................
+                ........................
+                .........fffff..........
+                ........f11111ff........
+                .......fb111111bf.......
+                .......f1111111dbf......
+                ......fd111111dddf......
+                ......fd11111ddddf......
+                ......fd11dddddddf......
+                ......f111dddddddf......
+                ......f11fcddddddf......
+                .....fb1111bdddbf.......
+                .....f1b1bdfcfff........
+                .....fbfbffffffff.......
+                ......fffffffffff.ff....
+                ...........ffffffff.....
+                ........f1b1bffffff.....
+                ........fbfbffffff......
+                ........................
+                ........................
+                ........................
+                ........................
+                `],
+            500,
+            true
+            )
+        }
+        if (mySprite.y > value.y) {
+            value.vy = 5
+        } else {
+            value.vy = -5
+        }
+    }
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     mySprite,
@@ -74,6 +203,236 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
+function TargetRunAway () {
+    for (let DuckVelocity of sprites.allOfKind(SpriteKind.target)) {
+        if (mySprite.x > Duck.x) {
+            DuckVelocity.vx = -50
+            animation.runImageAnimation(
+            Duck,
+            [img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . b 5 5 b . . . 
+                . . . . . . b b b b b b . . . . 
+                . . . . . b b 5 5 5 5 5 b . . . 
+                . b b b b b 5 5 5 5 5 5 5 b . . 
+                . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+                . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+                . . b d 5 5 b 1 f f 5 4 4 c . . 
+                b b d b 5 5 5 d f b 4 4 4 4 b . 
+                b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+                c d d d c c b 5 5 5 5 5 5 5 b . 
+                c b d d d d d 5 5 5 5 5 5 5 b . 
+                . c d d d d d d 5 5 5 5 5 d b . 
+                . . c b d d d d d 5 5 5 b b . . 
+                . . . c c c c c c c c b b . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . b 5 b . . . 
+                . . . . . . . . . b 5 b . . . . 
+                . . . . . . b b b b b b . . . . 
+                . . . . . b b 5 5 5 5 5 b . . . 
+                . b b b b b 5 5 5 5 5 5 5 b . . 
+                . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+                . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+                . . b d 5 5 b 1 f f 5 4 4 c . . 
+                b b d b 5 5 5 d f b 4 4 4 4 4 b 
+                b d d c d 5 5 b 5 4 4 4 4 4 b . 
+                c d d d c c b 5 5 5 5 5 5 5 b . 
+                c b d d d d d 5 5 5 5 5 5 5 b . 
+                . c d d d d d d 5 5 5 5 5 d b . 
+                . . c b d d d d d 5 5 5 b b . . 
+                . . . c c c c c c c c b b . . . 
+                `,img`
+                . . . . . . . . . . b 5 b . . . 
+                . . . . . . . . . b 5 b . . . . 
+                . . . . . . . . . b c . . . . . 
+                . . . . . . b b b b b b . . . . 
+                . . . . . b b 5 5 5 5 5 b . . . 
+                . . . . b b 5 d 1 f 5 5 d f . . 
+                . . . . b 5 5 1 f f 5 d 4 c . . 
+                . . . . b 5 5 d f b d d 4 4 . . 
+                b d d d b b d 5 5 5 4 4 4 4 4 b 
+                b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+                b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+                c d d c d 5 5 b 5 5 5 5 5 5 b . 
+                c b d d c c b 5 5 5 5 5 5 5 b . 
+                . c d d d d d d 5 5 5 5 5 d b . 
+                . . c b d d d d d 5 5 5 b b . . 
+                . . . c c c c c c c c b b . . . 
+                `,img`
+                . . . . . . . . . . b 5 b . . . 
+                . . . . . . . . . b 5 b . . . . 
+                . . . . . . b b b b b b . . . . 
+                . . . . . b b 5 5 5 5 5 b . . . 
+                . . . . b b 5 d 1 f 5 d 4 c . . 
+                . . . . b 5 5 1 f f d d 4 4 4 b 
+                . . . . b 5 5 d f b 4 4 4 4 b . 
+                . . . b d 5 5 5 5 4 4 4 4 b . . 
+                . . b d d 5 5 5 5 5 5 5 5 b . . 
+                . b d d d d 5 5 5 5 5 5 5 5 b . 
+                b d d d b b b 5 5 5 5 5 5 5 b . 
+                c d d b 5 5 d c 5 5 5 5 5 5 b . 
+                c b b d 5 d c d 5 5 5 5 5 5 b . 
+                . b 5 5 b c d d 5 5 5 5 5 d b . 
+                b b c c c d d d d 5 5 5 b b . . 
+                . . . c c c c c c c c b b . . . 
+                `,img`
+                . . . . . . . . . . b 5 b . . . 
+                . . . . . . . . . b 5 b . . . . 
+                . . . . . . b b b b b b . . . . 
+                . . . . . b b 5 5 5 5 5 b . . . 
+                . . . . b b 5 d 1 f 5 d 4 c . . 
+                . . . . b 5 5 1 f f d d 4 4 4 b 
+                . . . . b 5 5 d f b 4 4 4 4 b . 
+                . . . b d 5 5 5 5 4 4 4 4 b . . 
+                . b b d d d 5 5 5 5 5 5 5 b . . 
+                b d d d b b b 5 5 5 5 5 5 5 b . 
+                c d d b 5 5 d c 5 5 5 5 5 5 b . 
+                c b b d 5 d c d 5 5 5 5 5 5 b . 
+                c b 5 5 b c d d 5 5 5 5 5 5 b . 
+                b b c c c d d d 5 5 5 5 5 d b . 
+                . . . . c c d d d 5 5 5 b b . . 
+                . . . . . . c c c c c b b . . . 
+                `,img`
+                . . . . . . . . . . b 5 b . . . 
+                . . . . . . . . . b 5 b . . . . 
+                . . . . . . b b b b b b . . . . 
+                . . . . . b b 5 5 5 5 5 b . . . 
+                . . . . b b 5 d 1 f 5 5 d f . . 
+                . . . . b 5 5 1 f f 5 d 4 c . . 
+                . . . . b 5 5 d f b d d 4 4 . . 
+                . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+                b d d d b b d 5 5 4 4 4 4 4 b . 
+                b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+                c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+                c b d c d 5 5 b 5 5 5 5 5 5 b . 
+                . c d d c c b d 5 5 5 5 5 d b . 
+                . . c b d d d d d 5 5 5 b b . . 
+                . . . c c c c c c c c b b . . . 
+                . . . . . . . . . . . . . . . . 
+                `],
+            500,
+            true
+            )
+        } else {
+            DuckVelocity.vx = 50
+            animation.runImageAnimation(
+            Duck,
+            [img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . b 5 5 b . . . . . . . . . 
+                . . . . b b b b b b . . . . . . 
+                . . . b 5 5 5 5 5 b b . . . . . 
+                . . b 5 5 5 5 5 5 5 b b b b b . 
+                . . b 5 5 5 5 5 5 5 5 b 5 d b . 
+                . . f 4 d 5 f 1 d 5 b 5 5 b . . 
+                . . c 4 4 5 f f 1 b 5 5 d b . . 
+                . b 4 4 4 4 b f d 5 5 5 b d b b 
+                b 4 4 4 4 4 4 5 b 5 5 d c d d b 
+                . b 5 5 5 5 5 5 5 b c c d d d c 
+                . b 5 5 5 5 5 5 5 d d d d d b c 
+                . b d 5 5 5 5 5 d d d d d d c . 
+                . . b b 5 5 5 d d d d d b c . . 
+                . . . b b c c c c c c c c . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . b 5 b . . . . . . . . . . 
+                . . . . b 5 b . . . . . . . . . 
+                . . . . b b b b b b . . . . . . 
+                . . . b 5 5 5 5 5 b b . . . . . 
+                . . b 5 5 5 5 5 5 5 b b b b b . 
+                . . b 5 5 5 5 5 5 5 5 b 5 d b . 
+                . . f 4 d 5 f 1 d 5 b 5 5 b . . 
+                . . c 4 4 5 f f 1 b 5 5 d b . . 
+                b 4 4 4 4 4 b f d 5 5 5 b d b b 
+                . b 4 4 4 4 4 5 b 5 5 d c d d b 
+                . b 5 5 5 5 5 5 5 b c c d d d c 
+                . b 5 5 5 5 5 5 5 d d d d d b c 
+                . b d 5 5 5 5 5 d d d d d d c . 
+                . . b b 5 5 5 d d d d d b c . . 
+                . . . b b c c c c c c c c . . . 
+                `,img`
+                . . . b 5 b . . . . . . . . . . 
+                . . . . b 5 b . . . . . . . . . 
+                . . . . . c b . . . . . . . . . 
+                . . . . b b b b b b . . . . . . 
+                . . . b 5 5 5 5 5 b b . . . . . 
+                . . f d 5 5 f 1 d 5 b b . . . . 
+                . . c 4 d 5 f f 1 5 5 b . . . . 
+                . . 4 4 d d b f d 5 5 b . . . . 
+                b 4 4 4 4 4 5 5 5 d b b d d d b 
+                . b 4 4 4 4 4 5 5 b 5 5 5 d b b 
+                . . b 5 5 5 5 5 d 5 5 5 5 c d b 
+                . b 5 5 5 5 5 5 b 5 5 d c d d c 
+                . b 5 5 5 5 5 5 5 b c c d d b c 
+                . b d 5 5 5 5 5 d d d d d d c . 
+                . . b b 5 5 5 d d d d d b c . . 
+                . . . b b c c c c c c c c . . . 
+                `,img`
+                . . . b 5 b . . . . . . . . . . 
+                . . . . b 5 b . . . . . . . . . 
+                . . . . b b b b b b . . . . . . 
+                . . . b 5 5 5 5 5 b b . . . . . 
+                . . c 4 d 5 f 1 d 5 b b . . . . 
+                b 4 4 4 d d f f 1 5 5 b . . . . 
+                . b 4 4 4 4 b f d 5 5 b . . . . 
+                . . b 4 4 4 4 5 5 5 5 d b . . . 
+                . . b 5 5 5 5 5 5 5 5 d d b . . 
+                . b 5 5 5 5 5 5 5 5 d d d d b . 
+                . b 5 5 5 5 5 5 5 b b b d d d b 
+                . b 5 5 5 5 5 5 c d 5 5 b d d c 
+                . b 5 5 5 5 5 5 d c d 5 d b b c 
+                . b d 5 5 5 5 5 d d c b 5 5 b . 
+                . . b b 5 5 5 d d d d c c c b b 
+                . . . b b c c c c c c c c . . . 
+                `,img`
+                . . . b 5 b . . . . . . . . . . 
+                . . . . b 5 b . . . . . . . . . 
+                . . . . b b b b b b . . . . . . 
+                . . . b 5 5 5 5 5 b b . . . . . 
+                . . c 4 d 5 f 1 d 5 b b . . . . 
+                b 4 4 4 d d f f 1 5 5 b . . . . 
+                . b 4 4 4 4 b f d 5 5 b . . . . 
+                . . b 4 4 4 4 5 5 5 5 d b . . . 
+                . . b 5 5 5 5 5 5 5 d d d b b . 
+                . b 5 5 5 5 5 5 5 b b b d d d b 
+                . b 5 5 5 5 5 5 c d 5 5 b d d c 
+                . b 5 5 5 5 5 5 d c d 5 d b b c 
+                . b 5 5 5 5 5 5 d d c b 5 5 b c 
+                . b d 5 5 5 5 5 d d d c c c b b 
+                . . b b 5 5 5 d d d c c . . . . 
+                . . . b b c c c c c . . . . . . 
+                `,img`
+                . . . b 5 b . . . . . . . . . . 
+                . . . . b 5 b . . . . . . . . . 
+                . . . . b b b b b b . . . . . . 
+                . . . b 5 5 5 5 5 b b . . . . . 
+                . . f d 5 5 f 1 d 5 b b . . . . 
+                . . c 4 d 5 f f 1 5 5 b . . . . 
+                . . 4 4 d d b f d 5 5 b . . . . 
+                b 4 4 4 4 4 5 5 5 5 5 d b b b . 
+                . b 4 4 4 4 4 5 5 d b b d d d b 
+                . . b 5 5 5 5 5 5 b 5 5 5 d b b 
+                . b 5 5 5 5 5 5 d 5 5 5 5 c d c 
+                . b 5 5 5 5 5 5 b 5 5 d c d b c 
+                . b d 5 5 5 5 5 d b c c d d c . 
+                . . b b 5 5 5 d d d d d b c . . 
+                . . . b b c c c c c c c c . . . 
+                . . . . . . . . . . . . . . . . 
+                `],
+            500,
+            true
+            )
+        }
+        if (mySprite.y > Duck.y) {
+            DuckVelocity.vy = -50
+        } else {
+            DuckVelocity.vy = 50
+        }
+    }
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
@@ -334,8 +693,15 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
-let Ghost: Sprite = null
+sprites.onOverlap(SpriteKind.Player, SpriteKind.target, function (sprite, otherSprite) {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false)
+})
 let projectile: Sprite = null
+let Duck: Sprite = null
+let Ghost: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
@@ -358,7 +724,7 @@ mySprite = sprites.create(img`
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
 tiles.setCurrentTilemap(tilemap`level1`)
-game.onUpdateInterval(2000, function () {
+for (let index = 0; index < 4; index++) {
     Ghost = sprites.create(img`
         ........................
         ........................
@@ -386,4 +752,27 @@ game.onUpdateInterval(2000, function () {
         ........................
         `, SpriteKind.Enemy)
     Ghost.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+}
+Duck = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.target)
+Duck.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+game.onUpdate(function () {
+    GhostFollow()
+    TargetRunAway()
 })
